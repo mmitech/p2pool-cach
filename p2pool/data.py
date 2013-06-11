ac975fe0605f978a202b6c1aa44849c711403841
Rav3nPL <rav3n.pl@gmail.com>
2013-06-12 00:07:46 +0300

    Update data.py
    cut block-stale error

--- p2pool/data.py
+++ p2pool/data.py
@@ -444,7 +444,7 @@ class Share(object):
 
     def should_punish_reason(self, previous_block, bits, tracker, known_txs):
         if (self.header['previous_block'], self.header['bits']) != (previous_block, bits) and self.header_hash != previous_block and self.peer_addr is not None:
-            return True, 'Block-stale detected! %x < %x' % (self.header['previous_block'], previous_block)
+            return True, 'Block-stale detected!'
 
         if self.pow_hash <= self.header['bits'].target:
             return -1, 'block solution'
