30b2804da02ec8a01af9e6388bb706ac705309cc
Sykh <Sykh@users.noreply.github.com>
2014-01-21 11:46:21 +0100

    Data for cachecoin

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -8,42 +8,22 @@ from p2pool.util import math
 # changes can be done by changing one, then the other
 
 nets = dict(
-    yacoin=math.Object(
-        PARENT=networks.nets['yacoin'],
-        SHARE_PERIOD=10, # seconds
-        CHAIN_LENGTH=12*60*60//10, # shares
-        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+    cachecoin=math.Object(
+        PARENT=networks.nets['cachecoin'],
+        SHARE_PERIOD=120, # seconds
+        CHAIN_LENGTH=12*60*60//120, # shares
+        REAL_CHAIN_LENGTH=12*60*60//120, # shares
         TARGET_LOOKBEHIND=30, # shares
-        SPREAD=10, # blocks
-        IDENTIFIER='c138eee9e7923514'.decode('hex'),
-        PREFIX='d206c3aaaee749b4'.decode('hex'),
+        SPREAD=30, # blocks
+        IDENTIFIER='Q28kFRCjXjMYBfJy'.decode('hex'),
+        PREFIX='wVWQvM438v6L2mZr'.decode('hex'),
         P2P_PORT=8337,
         MIN_TARGET=0,
         MAX_TARGET=2**256//2**20 - 1,
-        PERSIST=True,
+        PERSIST=False,
         WORKER_PORT=8336,
         BOOTSTRAP_ADDRS='rav3n.dtdns.net 37.59.119.242 95.138.185.176 213.239.207.114 81.17.30.121 46.163.105.201 88.190.223.101'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-alt',
-        VERSION_CHECK=lambda v: v >= 60004,
-    ),
-
-    yacoin_testnet=math.Object(
-        PARENT=networks.nets['yacoin_testnet'],
-        SHARE_PERIOD=3, # seconds
-        CHAIN_LENGTH=20*60//3, # shares
-        REAL_CHAIN_LENGTH=20*60//3, # shares
-        TARGET_LOOKBEHIND=200, # shares
-        SPREAD=12, # blocks
-        IDENTIFIER='e037d5b8c7923510'.decode('hex'),
-        PREFIX='7208c1a54ef649b0'.decode('hex'),
-        P2P_PORT=19777,
-        MIN_TARGET=0,
-        MAX_TARGET=2**256//2**20 - 1,
-        PERSIST=False,
-        WORKER_PORT=18336,
-        BOOTSTRAP_ADDRS=' '.split(' '),
-        ANNOUNCE_CHANNEL='#p2pool-alt',
-        VERSION_CHECK=lambda v: v >= 60004,
     ),
 )
 for net_name, net in nets.iteritems():
