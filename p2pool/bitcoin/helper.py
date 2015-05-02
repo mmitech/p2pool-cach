61fd5957399f7991a531670cac9d7907559d956f
vertoe <vertoe@qhor.net>
2015-05-02 23:51:40 +0200

    Fix timestamps.

--- p2pool/bitcoin/helper.py
+++ p2pool/bitcoin/helper.py
@@ -39,16 +39,6 @@ def getwork(bitcoind, use_getblocktemplate=False):
             print >>sys.stderr, 'Error: Bitcoin version too old! Upgrade to v0.5 or newer!'
             raise deferral.RetrySilentlyException()
     packed_transactions = [(x['data'] if isinstance(x, dict) else x).decode('hex') for x in work['transactions']]
-
-    transactions=map(bitcoin_data.tx_type.unpack, packed_transactions)
-    transaction_hashes=map(bitcoin_data.hash256, packed_transactions)
-    
-    txn_timestamp = 0
-    
-    for tx in transactions:
-        if tx.timestamp > txn_timestamp:
-            txn_timestamp = tx.timestamp
-
     if 'height' not in work:
         work['height'] = (yield bitcoind.rpc_getblock(work['previousblockhash']))['height'] + 1
     elif p2pool.DEBUG:
@@ -56,12 +46,11 @@ def getwork(bitcoind, use_getblocktemplate=False):
     defer.returnValue(dict(
         version=work['version'],
         previous_block=int(work['previousblockhash'], 16),
-        transactions=transactions,
-        transaction_hashes=transaction_hashes,
+        transactions=map(bitcoin_data.tx_type.unpack, packed_transactions),
+        transaction_hashes=map(bitcoin_data.hash256, packed_transactions),
         transaction_fees=[x.get('fee', None) if isinstance(x, dict) else None for x in work['transactions']],
         subsidy=work['coinbasevalue'],
         time=work['time'] if 'time' in work else work['curtime'],
-        txn_timestamp=txn_timestamp,
         bits=bitcoin_data.FloatingIntegerType().unpack(work['bits'].decode('hex')[::-1]) if isinstance(work['bits'], (str, unicode)) else bitcoin_data.FloatingInteger(work['bits']),
         coinbaseflags=work['coinbaseflags'].decode('hex') if 'coinbaseflags' in work else ''.join(x.decode('hex') for x in work['coinbaseaux'].itervalues()) if 'coinbaseaux' in work else '',
         height=work['height'],
@@ -82,7 +71,10 @@ def submit_block_p2p(block, factory, net):
 def submit_block_rpc(block, ignore_failure, bitcoind, bitcoind_work, net):
 
     if bitcoind_work.value['use_getblocktemplate']:
-        result = yield bitcoind.rpc_submitblock(bitcoin_data.block_type.pack(block).encode('hex'))
+        try:
+            result = yield bitcoind.rpc_submitblock(bitcoin_data.block_type.pack(block).encode('hex'))
+        except jsonrpc.Error_for_code(-32601): # Method not found, for older litecoin versions
+            result = yield bitcoind.rpc_getblocktemplate(dict(mode='submit', data=bitcoin_data.block_type.pack(block).encode('hex')))
         success = result is None
     else:
         print bitcoin_data.block_type.pack(block).encode('hex')
